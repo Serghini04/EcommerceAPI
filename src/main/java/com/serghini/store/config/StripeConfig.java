7c87efba29f1fb749e748d6ba593f6ad013bbf50
Mehdi Serghini <91221718+Serghini04@users.noreply.github.com>
2025-08-21 17:44:26 +0000

    Start adding Stripe integration to checkout process

---
+++ src/main/java/com/serghini/store/config/StripeConfig.java
@@ -0,0 +1,15 @@
+package com.serghini.store.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StripeConfig {
+    @Value("${stripe.secretKey}")
+    private String secretKey;
+
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = secretKey;
+    }
+}
