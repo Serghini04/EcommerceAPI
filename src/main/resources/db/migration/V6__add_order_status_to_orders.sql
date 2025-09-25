d47200fa8c1edd227c0f0312a809260b7173d4c9
Mehdi Serghini <91221718+Serghini04@users.noreply.github.com>
2025-09-25 22:35:33 +0000

    Push Images

--- src/main/resources/db/migration/V6__add_order_status_to_orders.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE orders ADD COLUMN order_status VARCHAR(20) DEFAULT 'PENDING' NOT NULL AFTER customer_id;
--- src/main/resources/db/migration/V7__add_payment_intent_id_to_orders.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE orders ADD COLUMN payment_intent_id VARCHAR(100) DEFAULT NULL AFTER order_status;
